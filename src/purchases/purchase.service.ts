6a49a5c196e32fe950762ef92bfdc9088d46cc77
event <event@z2t7c3.42bangkok.com>
2026-03-25 12:12:39 +0700

    jiwerfowef

--- src/purchases/purchase.service.ts
+++ src/purchases/purchase.service.ts
@@ -1,6 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 import { Purchase } from './purchase.interface';
 import * as fs from 'fs';
+import { SourceCode } from 'eslint';
 
 export class PurchaseService {
   findAll(): Purchase {
@@ -13,11 +14,17 @@ export class PurchaseService {
     const file = fs.readFileSync('./data/purchases.json', 'utf-8');
     const datas: Purchase[] = JSON.parse(file);
     const userData = [...datas];
-    let result = [];
+    const result = [];
 
     for (let i = 0; i < userData.length; i++) {
       if (userData[i].id == id) {
         result.push(userData[i]);
+      }else{
+        return ({
+          success: false,
+          data: null,
+          message: `Purchase with id ${id} not found`
+        })
       }
       return result;
     }
