ea9893f6777f3c8a3ccbe2b8d34fe7539610f974
event <event@z2t7c3.42bangkok.com>
2026-03-25 11:13:52 +0700

    1, 2

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { ApiResponse } from 'src/interfaces/response.interface';
 
 @Controller()
 export class ProductController {
