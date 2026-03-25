b83f67696d8b08ce9ca75c54ca53ba1869eb2faf
event <event@z2t7c3.42bangkok.com>
2026-03-25 11:26:41 +0700

    gjie

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiResponse } from './interfaces/response.interface';
 
 @Controller()
 export class AppController {
